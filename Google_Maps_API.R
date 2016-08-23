a08bf087b315a9ae17dbd709769f8f93da14eb44
Christopher <gaschrish@yahoo.in>
2016-08-23 11:32:14 +0100

    Add files via upload

---
+++ Google_Maps_API.R
@@ -0,0 +1,27 @@
+# set working directory
+#setwd("C:/Users/AMULGASPAR/Desktop/VijayAnnaProject/Datasets")
+
+# Read the file
+taxi <- read.csv("taxi.csv",header=TRUE)
+
+# Convert latitude and logitude to address using Google Map API
+library(evaluate)
+install.packages("ggmap")
+install.packages("RJSONIO")
+library(RJSONIO)
+library(ggmap)
+
+# Create the Column that stores Pickup address
+taxi$Pickup_Address <- mapply(FUN=function(lon,lat)
+								revgeocode(c(lon,lat)),
+									taxi$Pickup_longitude,taxi$Pickup_latitude)
+
+taxi$Dropoff_Address <- mapply(FUN=function(lon,lat)
+								revgeocode(c(lon,lat)),
+									taxi$Dropoff_longitude,taxi$Dropoff_latitude)
+									
+									
+# Write the ouput in excel file
+library(xlsx)
+write.xlsx(taxi,file="taxi1.xlsx",sheetName="sheet1",row.names=FALSE)
+
